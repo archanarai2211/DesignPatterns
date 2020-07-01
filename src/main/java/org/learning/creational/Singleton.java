c6b559c2ac2650ab21cabd7c782651472b7f0979
Archana Rai <archana.rai@sap.com>
2020-07-01 12:43:55 +0530

    Singleton
    Change-Id: I07d63a3d5a25907d2415b5bd1d3030839e49da54
    Signed-off-by: Archana Rai <archana.rai@sap.com>

---
+++ src/main/java/org/learning/creational/Singleton.java
@@ -0,0 +1,41 @@
+package org.learning.creational;
+
+public class Singleton {
+	
+	//static for single reference and volatile for thread safe
+	//static variable get space in heap post java 8
+	//lazy loading so initialize with null
+	private static volatile Singleton instance = null;
+	
+	//private constructor to ensure that new instance can be created only through getInstance
+	//nobody can invoke constructor
+	//through error if invoked from reflection
+	private Singleton() {
+		if(instance != null)
+			throw new RuntimeException("Use getInstance() to create");
+	}
+
+	//Singleton pattern does not take arguments
+	//lazy loading so initializing it here
+	//Here, we run into a problem. Suppose that 
+	//there are two threads running. Both can get 
+	//inside of the if statement concurrently when 
+	//the instance is null. Then, one thread enters 
+	//the synchronized block to initialize the instance, 
+	//while the other is blocked. When the first thread exits 
+	//in the synchronized block, the waiting thread enters and 
+	//creates another singleton object. Note that when the 
+	//second thread enters the synchronized block, 
+	//it does not check to see if the instance is non-null.
+	public static Singleton getInstance() {
+		if (instance == null) {
+			synchronized (Singleton.class) {
+				if(instance == null) {
+					instance = new Singleton();
+				}
+			}
+			
+		}
+		return instance;
+	}
+}
